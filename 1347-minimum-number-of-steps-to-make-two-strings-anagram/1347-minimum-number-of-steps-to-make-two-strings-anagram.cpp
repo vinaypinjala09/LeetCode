39db9ad95a30420984fcd1eca7cbc808167add53
Vinay Pinjala <71168076+vinaypinjala09@users.noreply.github.com>
2024-01-13 14:51:17 +0530

    Time: 47 ms (81.78%), Space: 17 MB (36.03%) - LeetHub

--- 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.cpp
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.cpp
@@ -4,18 +4,16 @@ public:
         int count = 0;
         vector<int>vec(26,0);
         
-        for(char ch : s){
-            vec[ch-'a']++;
+        for(int i=0;i<s.length();i++){
+            vec[s[i]-'a']++;
+            vec[t[i]-'a']--;
         }
         
-        for(char ch:t){
-            if(vec[ch-'a']<1){
-                count++;
-            }
-            else{
-                vec[ch-'a']--;
-            }
-        }
+       for(int i:vec){
+           if(i>0){
+               count+=i;
+           }
+       }
         
         return count;
     }
