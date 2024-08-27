cd9615fd60b5650dc3b800fcf063f0da38b9bf8c
Vinay Pinjala <71168076+vinaypinjala09@users.noreply.github.com>
2024-08-27 12:27:19 +0530

    Time: 29 ms (6.42%), Space: 23.6 MB (5.09%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        
+        unordered_map<int,int> umap;
+        vector<int> ans;
+        for(int &i: nums){
+            umap[i]++;
+        }
+        
+        vector<vector<int>> freqArr;
+        
+        for(auto &i: umap){
+            freqArr.push_back({i.first,i.second});
+        }
+        
+        sort(freqArr.begin(), freqArr.end(), comp);
+        
+        for(int i=0;i<k;i++){
+            ans.push_back(freqArr[i][0]);
+        }
+        
+        return ans;
+        
+    }
+    
+    static bool comp(vector<int>a, vector<int>b){
+        if (a[1] > b[1]){
+            return true;
+        }
+        return false;
+    }
+};
